9a1bb942ee413f6f6f53f4a177e23bd9484dc90d
tdorsey <tdorsey@users.noreply.github.com>
2018-07-24 16:52:11 -0400

    Disable default pi user and copy custom ssh configs

--- firstrun.sh
+++ firstrun.sh
@@ -10,3 +10,14 @@ apt-get upgrade -y
 apt-get autoclean
 apt-get install
 echo "Package updates successful"
+
+echo "renaming pi user account to $USER"
+groupadd $USER
+usermod -d /home/$USER -m -g $USER -l $USER pi
+
+echo "copying custom ssh configs"
+cp /home/$USER/ssh_config /etc/ssh_config 
+cp /home/$USER/sshd_config /etc/sshd_config 
+
+echo "restarting ssh server"
+service restart sshd
