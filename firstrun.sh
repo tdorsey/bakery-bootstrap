7e8f7a22420ed58a7249cb77aa3f65a4e27d8568
tdorsey <tdorsey@users.noreply.github.com>
2018-07-24 17:45:12 -0400

    Recreate strong SSH host keys and add the default user to the SSH group

--- firstrun.sh
+++ firstrun.sh
@@ -2,7 +2,7 @@
 
 source .env; echo $USER; echo $PASS
 
-
+#Update packages
 echo "Performing initial setup tasks"
 echo "Updating packages"
 apt-get update -y
@@ -11,10 +11,21 @@ apt-get autoclean
 apt-get install
 echo "Package updates successful"
 
+#Rename default user
 echo "renaming pi user account to $USER"
 groupadd $USER
 usermod -d /home/$USER -m -g $USER -l $USER pi
 
+
+#Secure SSH
+groupadd ssh-users
+echo "reconfiguring SSH"
+usermod -aG ssh-users $USER
+cd /etc/ssh
+rm ssh_host_*key*
+ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N "" < /dev/null
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -N "" < /dev/null
+cd /home/$USER
 echo "copying custom ssh configs"
 cp /home/$USER/ssh_config /etc/ssh_config 
 cp /home/$USER/sshd_config /etc/sshd_config 
