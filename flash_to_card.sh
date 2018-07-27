02ec83d66004cab65c005541f5abb46cbf05cd74
Tommy Dorsey <tdorsey@users.noreply.github.com>
2018-07-26 21:02:00 -0400

    Flash device with cloud-init file

--- flash_to_card.sh
+++ flash_to_card.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#	FLASH_VERSION=2.1.1
-#HYPRIOT_VERSION=1.9.0
+FLASH_VERSION=2.1.1
+HYPRIOT_VERSION=1.9.0
 
-if test "$#" -ne 2
+if tes t "$#" -ne 2
  then
-    echo "Usage: $0 <FLASH_VERSION> <HYPRIOT_VERSION>"
+    echo "Usage: $0 <CLOUD-INIT> <BLOCK DEVICE>"
     exit 1
 fi
 
@@ -12,14 +12,13 @@ fi
 sudo apt-get install -y pv curl python-pip unzip hdparm
 
 FLASH_PATH='/usr/local/bin/flash'
-echo $FLASH_PATH
-FLASH_VERSION=$1
-HYPRIOT_SEMVER=$2
 #Install flash
-curl -LO https://github.com/hypriot/flash/releases/download/$1/flash
+curl -LO https://github.com/hypriot/flash/releases/download/$FLASH_VERSION/flash
 chmod +x flash
 sudo mv flash $FLASH_PATH
 
-flash https://github.com/hypriot/image-builder-rpi/releases/download/v$HYPRIOT_SEMVER/hypriotos-rpi-v$HYPRIOT_SEMVER.img.zip
+#Flash the drive
+flash -u $1 -d $2 https://github.com/hypriot/image-builder-rpi/releases/download/v$HYPRIOT_SEMVER/hypriotos-rpi-v$HYPRIOT_VERSION.img.zip
+
 
 
