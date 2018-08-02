de7375795dd213ee2140171c045c065d7e303725
Tommy Dorsey <tdorsey@users.noreply.github.com>
2018-08-02 16:35:33 -0400

    Only import ssh key

--- runcmd.sh
+++ runcmd.sh
@@ -11,8 +11,9 @@ rm -f /etc/ssh/ssh_host_rsa_key*
 rm -f /etc/ssh/ssh_host_ed25519_key*
 
 #Some OSes don't have the import ssh module, run it here after the package has been manually installed
-ssh-import-id "gh:tdorsey"
-
+echo "Importing SSH ID"
+ssh-import-id gh:tdorsey
+exit 0
 #Generate new host key with 100 rounds
 #No passphrase, hide output
 ssh-keygen -a 100 -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N "" < /dev/null
